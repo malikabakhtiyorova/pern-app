a98401fb52fb852351f77d34a86edcbd526cb8dd
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-04-19 14:13:21 +0500

    Setup express routes

--- server/db/index.js
+++ server/db/index.js
@@ -4,3 +4,4 @@ const pool = new Pool();
 module.exports = {
     query: (text,params) => pool.query(text,params)
 }
+
--- server/server.js
+++ server/server.js
@@ -3,23 +3,56 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const db = require('./db');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4000;
 
 app.use(cors())
 
 app.get('/api/v1/restaurants', async(req,res) => {
-    const {rows} = await db.query('select * from restaurants')
     // res.send('bismillah').end(200)
-    res.send(rows).status(200);
+    try{
+       const {rows} = await db.query('select * from restaurants')
+    res.json({
+        resStatus: 'success',
+        length: rows.length,
+        restaurants: rows
+    }).status(200);
+   }
+   catch(e){
+    res.send(e.message)
+   }
 })
 
-app.get('/api/v1/restaurants/:restaurantId', (req,res) => {
-    res.send(req.params)
+app.get('/api/v1/restaurants/:id', async(req,res) => {
+    let {id} = req.params
+      try{
+       const {rows:[row]} = await db.query('select * from restaurants where id = $1', [id])
+    res.json({
+        resStatus: 'success',
+        restaurant: row
+    }).status(200);
+   }
+   catch(e){
+    res.send(e.message)
+   }
 })
 
-app.post('/api/v1/restaurants', (req,res) => {
-    console.log(req.body)
+app.post('/api/v1/restaurants', async(req,res) => {
+     
+    const {name,location,price_range} = req.body
+      try{
+       const {rows:[row]} = await db.query('insert into restaurants(name,location,price_range) values($1,$2,$3) returning *;', [name,location,price_range])
+    res.json({
+        resStatus: 'success',
+        restaurant: row
+    }).status(200);
+   }
+   catch(e){
+    res.send(e.message)
+   }
 })
 
 app.put('/api/v1/restaurants/:id', (req,res) => {
